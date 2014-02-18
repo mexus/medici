0aeda1681b024535536e0601e62827b8e47cf3e1
mexus <gilaldpellaeon@gmail.com>
2014-02-18 16:52:31 +0400

    Medici deck calculator

---
+++ sources/dream_hacking/calculator.cpp
@@ -0,0 +1,11 @@
+#include "calculator.h"
+
+namespace dream_hacking {
+
+        Calculator::Calculator() {
+        }
+
+        Calculator::~Calculator() {
+        }
+
+} // namespace dream_hacking
---
+++ sources/dream_hacking/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define	CALCULATOR_H
+
+#include "../patience/medici.h"
+
+namespace dream_hacking {
+        class Calculator {
+        public:
+                Calculator();
+                virtual ~Calculator();
+        private:
+
+        };
+} //namespace dream_hacking
+
+#endif	/* CALCULATOR_H */
