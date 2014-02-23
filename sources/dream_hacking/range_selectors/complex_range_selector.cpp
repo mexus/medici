7837e68636fb63eff13b0c4cadb91e925da45079
Mexus <gilaldpellaeon@gmail.com>
2014-02-23 20:33:42 +0400

    Changed behaviour on no selectors

--- sources/dream_hacking/range_selectors/complex_range_selector.cpp
+++ sources/dream_hacking/range_selectors/complex_range_selector.cpp
@@ -9,37 +9,41 @@ namespace dream_hacking {
         }
 
         bool ComplexRangeSelector::Test(const std::vector<PlayingCard>& deck) const{
-                auto localSelectors = selectors;
-                
-                size_t len = deck.size();
-                for (size_t i = start; i < len; ++i){
-                        const PlayingCard& card = deck[i];
-                        auto it = localSelectors.begin();
-                        while (it != localSelectors.end()){
-                                const PRangeSelector& selector = *it;
-                                if (i > selector->to){
-                                        if (!selector->DefaultReturn())
-                                                return false;
-                                        it = localSelectors.erase(it);
-                                } else if (i >= selector->from){
-                                        auto res = selector->CheckCard(card);
-                                        if (res == RangeSelector::Error)
-                                                return false;
-                                        else if (res == RangeSelector::Ok)
+                if (selectors.empty()){
+                        return true;
+                } else {
+                        auto localSelectors = selectors;
+
+                        size_t len = deck.size();
+                        for (size_t i = start; i < len; ++i){
+                                const PlayingCard& card = deck[i];
+                                auto it = localSelectors.begin();
+                                while (it != localSelectors.end()){
+                                        const PRangeSelector& selector = *it;
+                                        if (i > selector->to){
+                                                if (!selector->DefaultReturn())
+                                                        return false;
                                                 it = localSelectors.erase(it);
-                                        else
+                                        } else if (i >= selector->from){
+                                                auto res = selector->CheckCard(card);
+                                                if (res == RangeSelector::Error)
+                                                        return false;
+                                                else if (res == RangeSelector::Ok)
+                                                        it = localSelectors.erase(it);
+                                                else
+                                                        ++it;
+                                        } else
                                                 ++it;
-                                } else
-                                        ++it;
+                                }
                         }
+
+                        for (auto it = localSelectors.begin(); it != localSelectors.end(); ++it){
+                                PRangeSelector& selector = *it;
+                                if (!selector->DefaultReturn())
+                                        return false;
+                        }
+                        return true;
                 }
-                
-                for (auto it = localSelectors.begin(); it != localSelectors.end(); ++it){
-                        PRangeSelector& selector = *it;
-                        if (!selector->DefaultReturn())
-                                return false;
-                }
-                return true;
         }
         
         void ComplexRangeSelector::AddRangeSelectors() {
